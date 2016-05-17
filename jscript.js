e6d23da9289658f0b2d7768742bc86032e69afe4
Kelley Parker <kelley.parker07@gmail.com>
2016-05-17 10:34:12 -0500

    Grid pen effect
    Grid pen effect

--- jscript.js
+++ jscript.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-		for (var x = 0; x < 16; x++) {
+
+	for (var x = 0; x < 16; x++) {
     	for (var y = 0; y < 16; y++) {
-      	var unit = $("<div class='grid'></div>");
+      	var unit = $("<div class='grid'></div> ");
 				$(".container").append(unit);
-      }
-    }
+	      	}
+      	$(".container").append("<br>");
+    		}
+
+    $(".grid").hover(function() {
+		$(this).css("background-color", "white");
+			}, function() {
+		$(this).css("background-color", "white");
+	});
+
 });
+
