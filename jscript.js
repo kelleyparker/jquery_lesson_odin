d4d93ab61d5eb1a51d811f3003f68fda5b37ee77
Kelley Parker <kelley.parker07@gmail.com>
2016-05-17 15:48:55 -0500

    got the grid and change grid on user input working
    yep

--- jscript.js
+++ jscript.js
@@ -1,18 +1,56 @@
+
+
 $(document).ready(function() {
 
 	for (var x = 0; x < 16; x++) {
-    	for (var y = 0; y < 16; y++) {
-      	var unit = $("<div class='grid'></div> ");
-				$(".container").append(unit);
-	      	}
-      	$(".container").append("<br>");
-    		}
+				var breakLine = $("<br>");
+				$(".container").append(breakLine);
+		    		for (var y = 0; y < 16; y++) {
+		      		var unit = $("<div class='grid'></div> ");
+					$(".container").append(unit);
+			     }
+		     
+		    $(".grid").hover(function() {
+				$(this).css("background-color", "red");
+					}, function() {
+				$(this).css("background-color", "white");
+			});
 
-    $(".grid").hover(function() {
-		$(this).css("background-color", "white");
-			}, function() {
-		$(this).css("background-color", "white");
+		    $(".buttonClear").hover(function() {
+		    	$(this).css("background-color","green");
+		    }, function () {
+		    	$(this).css("background-color", "lightgrey");
+		    });
+
+		}
+
+	$(".buttonClear").click(function(){
+		var numSide = prompt("How many sides do you want for your next grid?");
+
+		if (numSide > 30) {
+			var numSide = prompt("Too high, pick a lower number that's greater than 0");
+		}
+
+		if (numSide < 0) {
+
+			var numSide = prompt("Too low, pick a lower number that's lesser than 31");
+		}
+
+		$(".grid").remove()
+
+		for (var x = 0; x < numSide; x++) {
+				var breakLine = $("<br>");
+				$(".container").append(breakLine);
+		    		for (var y = 0; y < numSide; y++) {
+		      		var unit = $("<div class='grid'></div> ");
+					$(".container").append(unit);
+			     }
+			}
+
+			
+
 	});
+
 
 });
 
