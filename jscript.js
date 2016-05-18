31e50dc0d65ff0aa95af59990b6f0750b96e4e9a
Kelley Parker <kelley.parker07@gmail.com>
2016-05-17 21:31:59 -0500

    updated code
    took out the trailing <BRs> that kept coming up.  I can select boxes
    with color changes even after changing grid dimensions!!

--- jscript.js
+++ jscript.js
@@ -2,10 +2,11 @@
 
 $(document).ready(function() {
 
-	for (var x = 0; x < 16; x++) {
+	var numSide = 16;
+	for (var x = 0; x < numSide; x++) {
 				var breakLine = $("<br>");
 				$(".container").append(breakLine);
-		    		for (var y = 0; y < 16; y++) {
+		    		for (var y = 0; y < numSide; y++) {
 		      		var unit = $("<div class='grid'></div> ");
 					$(".container").append(unit);
 			     }
@@ -36,7 +37,8 @@ $(document).ready(function() {
 			var numSide = prompt("Too low, pick a lower number that's lesser than 31");
 		}
 
-		$(".grid").remove()
+		$(".grid").remove();
+		$("br").remove();
 
 		for (var x = 0; x < numSide; x++) {
 				var breakLine = $("<br>");
@@ -47,7 +49,17 @@ $(document).ready(function() {
 			     }
 			}
 
-			
+		 $(".grid").hover(function() {
+				$(this).css("background-color", "red");
+					}, function() {
+				$(this).css("background-color", "white");
+			});
+
+		    $(".buttonClear").hover(function() {
+		    	$(this).css("background-color","green");
+		    }, function () {
+		    	$(this).css("background-color", "lightgrey");
+		    });			
 
 	});
 
